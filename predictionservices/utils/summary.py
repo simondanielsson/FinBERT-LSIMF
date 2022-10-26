21bb37d2d038b3e03fac9cfe94b609f720549d0b
edinmsa <si7660da-s@student.lu.se>
2022-10-26 10:49:32 +0200

    Add validation loss to summary, and sort values wrt test loss

--- predictionservices/utils/summary.py
+++ predictionservices/utils/summary.py
@@ -21,6 +21,10 @@ def create_test_results() -> None:
         if 'test_metrics.csv' not in files:
             continue
 
+        # load validation loss
+        train_val_metrics = pd.read_csv(os.path.join(dir_path, "train_val_metrics.csv"), index_col=0)
+        val_loss = pd.Series({'val_loss': train_val_metrics.loc[:, 'val_loss'].min()})
+
         # load test metrics
         test_metrics = pd.read_csv(os.path.join(dir_path, 'test_metrics.csv')).squeeze()
 
@@ -29,11 +33,14 @@ def create_test_results() -> None:
             model_config = json.load(f)
         model_config_s = pd.Series(model_config)
 
+        # get the date from the last part of the path
         run_date = pd.Series({'run_date': dir_path.split('/')[-1][-11:]})
 
         # add to previous results
-        new_results = pd.concat([test_metrics, model_config_s, run_date])
+        new_results = pd.concat([test_metrics, val_loss, model_config_s, run_date])
         results = pd.concat([results, new_results], axis=1)
+
+    results = results.T.sort_values('loss')
 
     summary_path = get_test_summary_path()
 
@@ -43,6 +50,10 @@ def create_test_results() -> None:
 
 
 def update_test_summary(new_result_dirpath) -> None:
+    # train and validation metrics
+    train_val_metrics = pd.read_csv(os.path.join(new_result_dirpath, "train_val_metrics.csv"), index_col=0)
+    val_loss = pd.Series({'val_loss': train_val_metrics.loc[:, 'val_loss'].min()})
+
     # load test metrics
     test_metrics = pd.read_csv(os.path.join(new_result_dirpath, 'test_metrics.csv')).squeeze()
 
@@ -56,14 +67,18 @@ def update_test_summary(new_result_dirpath) -> None:
     current_results = pd.read_csv(get_test_summary_path())
 
     # add to previous results
-    new_results = pd.DataFrame(pd.concat([test_metrics, model_config_s, run_date])).T
+    new_results = pd.DataFrame(pd.concat([test_metrics, val_loss, model_config_s, run_date])).T
 
-    pd.concat([current_results, new_results]).sort_values('loss').to_csv(get_test_summary_path(), index=False)
+    pd.concat([current_results, new_results])\
+        .sort_values('loss')\
+        .to_csv(get_test_summary_path(), index=False)
 
     logging.info("Added experiment to test set experiment summary.")
 
 
 if __name__ == "__main__":
+    create_test_results()
+
     # print the current test set experimentation summary
     summary = pd.read_csv(get_test_summary_path())
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
